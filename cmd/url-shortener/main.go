fc490d62db61f8569133ef9ec347d2405e313798
aleksei_isaev <magnetichell@gmail.com>
2025-02-12 20:24:11 +0400

    added storage config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -24,10 +26,17 @@ func main() {
     log.Debug("debug message are enable")
 
     // TODO: init storage: sqlite
+    storage, err := sqlite.New(cfg.StoragePath)
+    if err != nil {
+        log.Error("failed to init storage", sl.Err(err))
+        os.Exit(1)
+    }
+
+    _ = storage
 
     // TODO: init router: chi, chi render
 
-    // TODO run server
+    // TODO: run server
 }
 
 func setupLogger(env string) *slog.Logger {
