e1137f763ad609eb1ee659adaaa67e7b00484938
aleksei_isaev <magnetichell@gmail.com>
2025-02-12 15:20:54 +0400

    added configs for envirement and logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,15 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+    envLocal = "local"
+    envDev = "dev"
+    envProd = "prod"
+)
+
 func main() {
     // TODO: init config: cleanenv
+    cfg := config.MustLoad()
+    fmt.Println(cfg)
 
     // TODO: init logger: slog import from "log/slog"
+    log := setupLogger(cfg.Env)
+    log.Info("start url-shortener", slog.String("env", cfg.Env))
+    log.Debug("debug message are enable")
 
     // TODO: init storage: sqlite
 
     // TODO: init router: chi, chi render
 
     // TODO run server
+}
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+
+    switch env {
+    case envLocal:
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envDev:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case envProd:
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+
+    return log
 }
 
 
